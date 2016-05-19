469d305054d0e67f45533bbde4069523aa97b71a
Marco Pereirinha <marco.pereirinha@metronet.no>
2016-05-19 12:24:59 +0100

    Abstract JS action method so it can be reused

--- assets/javascript/admin/admin.js
+++ assets/javascript/admin/admin.js
@@ -1,27 +1,31 @@
 (function(){
 	'use strict';
 
-	var metaImageFrame;
+	var metaImageFrame,
+		action = function(e) {
+			e.preventDefault();
 
-	document.getElementById('_cpwp_showcase_screenshot_button').addEventListener('click', function(e){
-		e.preventDefault();
+			if ( metaImageFrame ) {
+				metaImageFrame.open();
+				return;
+			}
 
-		if ( metaImageFrame ) {
+			metaImageFrame = wp.media.frames.meta_image_frame = wp.media({
+				'title'   : cpwpAdminLocalization.title,
+				'button'  : { 'text' : cpwpAdminLocalization.button },
+				'library' : { 'type' : 'image' }
+			});
+
+			metaImageFrame.on('select', function(){
+				var mediaAttachment = metaImageFrame.state().get('selection').first().toJSON();
+
+				document.getElementById('cpwp_showcase_screenshot_placeholder').style.backgroundImage = 'url("' + mediaAttachment.url + '")';
+				document.getElementById('_cpwp_showcase_screenshot').value = mediaAttachment.id;
+			});
+
 			metaImageFrame.open();
-			return;
-		}
+		};
 
-		metaImageFrame = wp.media.frames.meta_image_frame = wp.media({
-			title: cpwp_admin_localization.title,
-			button: { text: cpwp_admin_localization.button },
-			library: { type: 'image' }
-		});
-
-		metaImageFrame.on('select', function(){
-			var mediaAttachment = metaImageFrame.state().get('selection').first().toJSON();
-			document.getElementById('_cpwp_showcase_screenshot').val(mediaAttachment.url);
-		});
-
-		metaImageFrame.open();
-	});
+	document.getElementById('_cpwp_showcase_screenshot_button').addEventListener('click', action);
+	document.getElementById('cpwp_showcase_screenshot_placeholder').addEventListener('click', action);
 }());
