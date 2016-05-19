497cb7d2a7bdab376965b91864d6ad41f7181d5a
Marco Pereirinha <marco.pereirinha@metronet.no>
2016-05-19 12:26:27 +0100

    Add screenshot thumbnail preview to Detail box

--- includes/meta-fields.php
+++ includes/meta-fields.php
@@ -8,22 +8,31 @@ namespace CPWP\Showcase\Fields\Metafields;
  * @param post $post The post object
  */
 function showcase_details( $post ) {
-	wp_nonce_field( basename( __FILE__ ), 'showcase_details_nonce' );
+	wp_nonce_field( 'cpwp_showcase_details_nonce', 'showcase_details_nonce' );
 
 	$current_url        = get_post_meta( $post->ID, '_cpwp_showcase_url', true );
 	$current_screenshot = get_post_meta( $post->ID, '_cpwp_showcase_screenshot', true );
+	$screenshoot        = null;
+
+	if ( $current_screenshot ) {
+		$screenshoot = wp_get_attachment_image_src( $current_screenshot );
+		$screenshoot = $screenshoot[0];
+	}
 
 	?>
-	<p>
-		<label for="_cpwp_showcase_url"><?php esc_html_e( 'URL', 'cpwp_showcase' ); ?></label><br>
-		<input type="url" name="_cpwp_showcase_url" value="<?php echo esc_attr( $current_url ); ?>">
-	</p>
+	<div class="cpwp-showcase-url">
+		<label for="_cpwp_showcase_url"><?php esc_html_e( 'URL', 'cpwp_showcase' ); ?> <span title="<?php esc_attr_e( 'Required', 'cpwp_showcase' ); ?>" class="required">*</span></label><br>
+		<input type="url" name="_cpwp_showcase_url" value="<?php echo esc_attr( $current_url ); ?>" required>
+	</div>
 
-	<p>
-		<label for="_cpwp_showcase_screenshot"><?php esc_html_e( 'Screenshot', 'cpwp_showcase' ); ?></label><br>
-		<input type="text" name="_cpwp_showcase_screenshot" id="_cpwp_showcase_screenshot" value="<?php echo ! empty( $current_screenshot ) ? esc_attr( $current_screenshot[0] ) : ''; ?>" />
-		<input type="button" id="_cpwp_showcase_screenshot_button" class="button" value="<?php esc_html_e( 'Choose or Upload an Image', 'cpwp_showcase' )?>" />
-	</p>
+	<div class="cpwp-showcase-screenshot">
+		<label for="_cpwp_showcase_screenshot"><?php esc_html_e( 'Screenshot', 'cpwp_showcase' ); ?> <span title="<?php esc_attr_e( 'Required', 'cpwp_showcase' ); ?>" class="required">*</span></label><br>
+		<div id="cpwp_showcase_screenshot_placeholder" class="cpwp-showcase-screenshot-placeholder" style="background-image: url('<?php echo esc_url( $screenshoot ); ?>')"></div>
+		<input type="hidden" name="_cpwp_showcase_screenshot" id="_cpwp_showcase_screenshot" value="<?php echo ! empty( $current_screenshot ) ? esc_attr( $current_screenshot[0] ) : ''; ?>" required>
+		<p>
+			<input type="button" id="_cpwp_showcase_screenshot_button" class="button" value="<?php esc_html_e( 'Choose or Upload an Image', 'cpwp_showcase' )?>">
+		</p>
+	</div>
 
 	<?php
 }
