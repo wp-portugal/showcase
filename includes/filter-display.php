b211c6c5067da6ab11674b15a25d43a33dc60d94
rfvcorreia <rfvcorreia@gmail.com>
2016-05-23 17:19:04 +0100

    Added subject taxonomy Added subject filter Fix identification of filters only in main query Fix get current term for more than one taxonomy

--- includes/filter-display.php
+++ includes/filter-display.php
@@ -4,18 +4,19 @@ namespace CPWP\Showcase\Filter\Display;
 
 /**
  * Adds Year filter in Showcase Archive page before loop starts.
+ *
+ * @param  query $query Current WP_Query object.
  */
-function display_filters() {
+function display_filters( $query ) {
 
 	$cpwp_showcase_taxonomies = get_object_taxonomies( 'cpwp_showcase' );
 	$current_taxonomy = get_query_var( 'taxonomy' );
-	$current_term = get_query_var( 'term' );
 
-	if ( is_post_type_archive( 'cpwp_showcase' ) || in_array( $current_taxonomy, $cpwp_showcase_taxonomies, true ) ) {
+	if ( $query->is_main_query() && is_post_type_archive( 'cpwp_showcase' ) || in_array( $current_taxonomy, $cpwp_showcase_taxonomies, true ) ) {
 		?>
 		<form id="filter-showcase" class="filter-showcase" method="get">
 
-			<?php do_action( 'cpwp_showcase_filter', $current_term ); ?>
+			<?php do_action( 'cpwp_showcase_filter' ); ?>
 
 			<input type="submit" value="<?php esc_html_e( 'Filter', 'cpwp_showcase' ); ?>" />
 
---
+++ includes/tax-subject.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace CPWP\Showcase\Core\Taxonomy\Subject;
+
+/**
+ * Register year taxonomy
+ */
+function taxonomy() {
+	$labels = array(
+		'name'                       => _x( 'Subjects', 'Taxonomy General Name', 'cpwp_showcase' ),
+		'singular_name'              => _x( 'Subject', 'Taxonomy Singular Name', 'cpwp_showcase' ),
+		'menu_name'                  => __( 'Subjects', 'cpwp_showcase' ),
+		'all_items'                  => __( 'All Items', 'cpwp_showcase' ),
+		'new_item_name'              => __( 'New Item Name', 'cpwp_showcase' ),
+		'add_new_item'               => __( 'Add New Item', 'cpwp_showcase' ),
+		'edit_item'                  => __( 'Edit Item', 'cpwp_showcase' ),
+		'update_item'                => __( 'Update Item', 'cpwp_showcase' ),
+		'view_item'                  => __( 'View Item', 'cpwp_showcase' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'cpwp_showcase' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'cpwp_showcase' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'cpwp_showcase' ),
+		'popular_items'              => __( 'Popular Items', 'cpwp_showcase' ),
+		'search_items'               => __( 'Search Items', 'cpwp_showcase' ),
+		'not_found'                  => __( 'Not Found', 'cpwp_showcase' ),
+		'no_terms'                   => __( 'No items', 'cpwp_showcase' ),
+		'items_list'                 => __( 'Items list', 'cpwp_showcase' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'cpwp_showcase' ),
+	);
+
+	$rewrite = array(
+		'slug'                       => sanitize_title( _x( 'Subject', 'Taxonomy slug', 'cpwp_showcase' ) ),
+		'with_front'                 => true,
+		'hierarchical'               => true,
+	);
+
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+		'rewrite'                    => $rewrite,
+	);
+
+	register_taxonomy( 'cpwp_subject', array( 'cpwp_showcase' ), $args );
+}
+
+add_action( 'init', __NAMESPACE__ . '\taxonomy' );
