cec3a3281fc5cace8986e557b3e8e3d281cd7b7c
rfvcorreia <rfvcorreia@gmail.com>
2016-05-30 12:04:06 +0100

    Add localization to slugs and rewrite tags

--- includes/tax-permalinks.php
+++ includes/tax-permalinks.php
@@ -9,9 +9,12 @@ function add_rewrite_rules() {
 
 	global $wp_rewrite;
 
+	$year_slug = esc_html__( 'year', 'cpwp_showcase' );
+	$subject_slug = esc_html__( 'subject', 'cpwp_showcase' );
+
 	$new_rules = array(
-		'showcase/(year|subject)/(.+?)/(year|subject)/(.+?)/?$' => 'index.php?post_type=cpwp_showcase&cpwp_' . $wp_rewrite->preg_index( 1 ) . '=' . $wp_rewrite->preg_index( 2 ) . '&cpwp_' . $wp_rewrite->preg_index( 3 ) . '=' . $wp_rewrite->preg_index( 4 ),
-		'showcase/(year|subject)/(.+)/?$' => 'index.php?post_type=cpwp_showcase&cpwp_' . $wp_rewrite->preg_index( 1 ) . '=' . $wp_rewrite->preg_index( 2 ),
+		'showcase/('.$year_slug.'|'.$subject_slug.')/(.+?)/('.$year_slug.'|'.$subject_slug.')/(.+?)/?$' => 'index.php?post_type=cpwp_showcase&cpwp_' . $wp_rewrite->preg_index( 1 ) . '=' . $wp_rewrite->preg_index( 2 ) . '&cpwp_' . $wp_rewrite->preg_index( 3 ) . '=' . $wp_rewrite->preg_index( 4 ),
+		'showcase/('.$year_slug.'|'.$subject_slug.')/(.+)/?$' => 'index.php?post_type=cpwp_showcase&cpwp_' . $wp_rewrite->preg_index( 1 ) . '=' . $wp_rewrite->preg_index( 2 ),
 	);
 
 	$wp_rewrite->rules = $new_rules + $wp_rewrite->rules;
@@ -19,3 +22,15 @@ function add_rewrite_rules() {
 }
 
 add_action( 'generate_rewrite_rules', __NAMESPACE__ . '\add_rewrite_rules' );
+
+/**
+ * Adds custom rewrite tags
+ */
+function new_rewrite_tags() {
+
+	add_rewrite_tag( '%year%', '([^&]+)','cpwp_year=' );
+	add_rewrite_tag( '%subject%', '([^&]+)','cpwp_subject=' );
+
+}
+
+add_action( 'init', __NAMESPACE__ . '\new_rewrite_tags', 10, 0 );
